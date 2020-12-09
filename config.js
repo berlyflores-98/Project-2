612154c4ee2a1ba9df420ead226f215f2cd9b0ac
Kiran Mundae <kkmundae@gmail.com>
2020-12-08 19:59:45 -0600

    updates to map.html

---
+++ config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1Ijoia2ttdW5kYWUiLCJhIjoiY2tocGozYWQ1MDJuZjJ6cHRoaTN0bzA3MCJ9.RUOyrqyHteZNp-neii-aOQ";
--- static/js/map.js
+++ static/js/map.js
@@ -1,83 +1,26 @@
-// // Creating map object
-// var myMap = L.map("map", {
-//     center: [0, 0],
-//     zoom: 2
-//   });
-  
-//   // Adding tile layer
-//   L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-//     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-//     maxZoom: 18,
-//     id: "streets-v11",
-//     accessToken: API_KEY
-//   }).addTo(myMap);
-  
-  // // // If data.beta.nyc is down comment out this link
-  // // var link = "https://data.beta.nyc//dataset/0ff93d2d-90ba-457c-9f7e-39e47bf2ac5f/resource/" +
-  // // "35dd04fb-81b3-479b-a074-a27a37888ce7/download/d085e2f8d0b54d4590b1e7d1f35594c1pediacitiesnycneighborhoods.geojson";
-  
-  // // Uncomment this link local geojson for when data.beta.nyc is down
-  // var link = 'data/data.json';
-  
-  // // Function that will determine the color of a neighborhood based on the borough it belongs to
-  // // function chooseColor(borough) {
-  // //   switch (borough) {
-  // //   case "Brooklyn":
-  // //     return "yellow";
-  // //   case "Bronx":
-  // //     return "red";
-  // //   case "Manhattan":
-  // //     return "orange";
-  // //   case "Queens":
-  // //     return "green";
-  // //   case "Staten Island":
-  // //     return "purple";
-  // //   default:
-  // //     return "black";
-  // //   }
-  // // }
-  
-  // // Grabbing our GeoJSON data..
-  // d3.json(link).then(function(data) {
-  //   // Creating a geoJSON layer with the retrieved data
-  //   L.geoJson(data, {
-  //     // Style each feature (in this case a neighborhood)
-  //     style: function(feature) {
-  //       return {
-  //         color: "white",
-  //         // Call the chooseColor function to decide which color to color our neighborhood (color based on borough)
-  //         fillColor: chooseColor(feature.properties.borough),
-  //         fillOpacity: 0.5,
-  //         weight: 1.5
-  //       };
-  //     },
-  //     // Called on each feature
-  //     onEachFeature: function(feature, layer) {
-  //       // Set mouse events to change map styling
-  //       layer.on({
-  //         // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 90% so that it stands out
-  //         mouseover: function(event) {
-  //           layer = event.target;
-  //           layer.setStyle({
-  //             fillOpacity: 0.9
-  //           });
-  //         },
-  //         // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
-  //         mouseout: function(event) {
-  //           layer = event.target;
-  //           layer.setStyle({
-  //             fillOpacity: 0.5
-  //           });
-  //         },
-  //         // When a feature (neighborhood) is clicked, it is enlarged to fit the screen
-  //         click: function(event) {
-  //           map.fitBounds(event.target.getBounds());
-  //         }
-  //       });
-  //       // Giving each feature a pop-up with information pertinent to it
-  //       layer.bindPopup("<h1>" + feature.properties.neighborhood + "</h1> <hr> <h2>" + feature.properties.borough + "</h2>");
-  
-  //     }
-  //   }).addTo(myMap);
-  // });
-  
+// Creating map object
+var myMap = L.map("map", {
+  center: [34.0522, -118.2437],
+  zoom: 1.5
+});
+
+// Adding tile layer
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  id: 'mapbox/streets-v11',
+  tileSize: 512,
+  zoomOffset: -1,
+  accessToken: API_KEY
+}).addTo(myMap);
+
+//Bring in data for Netflix, GeoJson
+//var link = "static/data/nyc.geojson";
+
+//var choroplethLayer;
+
+// Grabbing our GeoJSON data..
+d3.json(link).then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data
+  L.geoJson(data).addTo(map);
+});
