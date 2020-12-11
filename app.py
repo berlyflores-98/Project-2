fe08930be9d69a48ba57cb4f381d4476cc94dd4c
Kimberly Flores <kimberlyflores@Kimberlys-MBP.localdomain>
2020-12-11 15:29:27 -0600

    updating app

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ import json
 # from pprint import pprint
 # from sqlalchemy import inspect
 
-# app = Flask(__name__)
-# app.config['JSON_SORT_KEYS'] = False
+app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
 
 # # # Database Set Up 
 # url = 'postgres://fdcftdhewdyqvh:28a4642cad69d69b6065c15fa11d97719397b94c0cfb460554560c80b81ce864@ec2-54-163-47-62.compute-1.amazonaws.com:5432/d15vhqvdffuqdg'
